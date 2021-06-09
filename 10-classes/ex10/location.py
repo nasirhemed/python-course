527b1250c3b2de45c3ed637fe95cb8abd012209b
Nasir Hemed <nasirhemed@ymail.com>
2021-06-09 06:13:37 -0600

    Update location exercise 10

--- 10-classes/ex10/location.py
+++ 10-classes/ex10/location.py
@@ -1,21 +1,27 @@
-import requests
+from urllib import request, parse
 import json
 
 API_KEY = "yhvQAeWy9SO0fNesp7oA63AyAEGCfRSM"
-URL = "http://open.mapquestapi.com/geocoding/v1/address"
+URL = "http://open.mapquestapi.com/geocoding/v1/address/"
 
 class Location:
 
     def __init__(self, addr):
+        
+        request_params = {
+            "key" : API_KEY,
+            "location": addr,
+            "maxResults": 1
+        }
 
-        request_url = "{}/?key={}&location={}&maxResults=1".format(URL, API_KEY, addr)
+        encoded_url = URL + "?" + parse.urlencode(request_params)
 
-        result = requests.get(request_url)
+        result = request.urlopen(encoded_url)
 
-        if result.status_code != 200:
+        if result.getcode() != 200:
             raise Exception("Address not found")
 
-        content = result.content
+        content = result.read()
         content_dict = json.loads(content)
 
         results = content_dict["results"][0]
@@ -28,5 +34,22 @@ class Location:
         self.city = location["adminArea5"]
         self.postalCode = location["postalCode"]
 
-    # Add a method to tell the user info about the location
-    # Add a method to tell the user about the postalCode
+
+    def __str__(self):
+
+        return """
+            City: {}
+            Province/State: {}
+            Country: {}
+            Postal Code: {}
+            """.format(self.city, self.state, self.country, self.postalCode)
+
+    def __eq__(self,other):
+
+        if not isinstance(other, Location):
+            return False
+
+        return self.city == other.city and self.province == other.province and self.country == other.country and self.postalCode == other.pastalCode
+
+a = Location("Square One, Mississauga")
+print(a)
